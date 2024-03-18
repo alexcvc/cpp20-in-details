603a520435b5d1dfc4141df6c62e1901e7caebfe
A.Sacharov <a.sacharov@gmx.de>
2024-03-18 13:37:26 +0100

    Improve feedback in erase-remove idiom vector operation
    The code has been refactored to detail the workings of the erase-remove idiom used in vector operations. Comprehensive comments have been added to explain how elements are reordered for removal, enhancing readability and understanding. This modification is useful for understanding both the concept and its application in C++.

--- Library/removeElements.cpp
+++ Library/removeElements.cpp
@@ -6,16 +6,22 @@
 
 int main() {
 
-    std::cout << '\n';
+   std::cout << '\n';
 
-    std::vector myVec{-2, 3, -5, 10, 3, 0, -5 };
+   std::vector myVec{-2, 3, -5, 10, 3, 0, -5};
 
-    for (auto ele: myVec) std::cout << ele << " ";
-    std::cout << "\n\n";
+   for (auto ele: myVec) std::cout << ele << " ";
+   std::cout << "\n\n";
+   /**
+    * What std::remove_if does is to reorder the elements in the range
+    * [myVec.begin(), myVec.end()] in such a way that all the elements
+    * for which the lambda function returns true are put at the end of
+    * the range. It does not physically remove them from the vector.
+    */
+   std::remove_if(myVec.begin(), myVec.end(), [](int ele) { return ele < 0; }); // (1)
 
-    std::remove_if(myVec.begin(), myVec.end(), [](int ele){ return ele < 0; }); // (1)
-    for (auto ele: myVec) std::cout << ele << " ";
+   for (auto ele: myVec) std::cout << ele << " ";
 
-    std::cout << "\n\n";
+   std::cout << "\n\n";
 
 }
